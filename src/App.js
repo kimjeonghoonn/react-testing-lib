16711b678c21d5169db17d555354bcd9ef47337e
kimjeonghoonn <jhun6241@daum.net>
2022-10-17 12:45:16 +0900

    Counter test ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Counter from './Counter';
 import Profile from './Profile';
 
 function App() {
   return (
       <div classeName="App">
-        <Profile username="kimjeonghoonn" name="김정훈" />
+        {/* <Profile username="kimjeonghoonn" name="김정훈" /> */}
+        <Counter />
         learn react
       </div>
     );
